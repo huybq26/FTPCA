bc9cdb86d62fdfd27d1208aaeeffecfd57683799
huybq26 <bui.quang.huy.2002@gmail.com>
2023-06-24 02:36:21 +0800

    Add frontend functionalities

--- frontend/src/app/messaging/MessageChat.tsx
+++ frontend/src/app/messaging/MessageChat.tsx
@@ -24,6 +24,12 @@ const MessageChat: React.FC = () => {
 		useState<Conversation | null>(null);
 	const [messages, setMessages] = useState<Message[]>([]);
 	const [newMessage, setNewMessage] = useState<string>('');
+	const [newChatName, setNewChatName] = useState<string>('');
+	const [showNewChatModal, setShowNewChatModal] = useState<boolean>(false);
+	const [searchedUsers, setSearchedUsers] = useState<UserInfo[]>([]);
+	const [selectedUsers, setSelectedUsers] = useState<UserInfo[]>([]);
+	const [searchQuery, setSearchQuery] = useState<string>('');
+
 	const navigate = useNavigate();
 	const messagesContainerRef = useRef<HTMLDivElement>(null);
 
@@ -124,134 +130,329 @@ const MessageChat: React.FC = () => {
 				console.log('Error:', response.status);
 			}
 		} catch (error) {
-			console.log(
-				`Error fetching messages for conversation ${conversation.id}:`,
-				error
-			);
+			console.log('An error occurred:', error);
 		}
 	};
 
-	const handleSendMessage = async () => {
-		// Add your logic to send the newMessage value
-		// and update the messages state accordingly
-		try {
-			const response = await fetch(
-				`http://localhost:5169/sendmessage?senderid=${userInfo?.userid}&convid=${selectedConversation?.id}&content=${newMessage}`,
-				{
-					method: 'POST',
-					headers: {
-						Authorization: `Bearer ${sessionStorage.getItem('jwtToken')}`,
-					},
-				}
-			);
+	const handleNewMessageChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNewMessage(event.target.value);
+	};
 
-			if (response.ok) {
-				console.log('Sent message successfully!');
-			} else {
-				console.log('Error:', response.status);
+	const handleSendMessage = async () => {
+		if (selectedConversation && newMessage.trim() !== '') {
+			try {
+				const response = await axios.post(
+					`http://localhost:5169/sendmessage?sender=${userInfo?.userid}&convid=${selectedConversation.id}&content=${newMessage}`,
+					{},
+					{
+						headers: {
+							Authorization: `Bearer ${sessionStorage.getItem('jwtToken')}`,
+						},
+					}
+				);
+
+				if (response.status === 200) {
+					const newMessageObject: Message = {
+						id: response.data.messageid,
+						sender: userInfo?.userid || '',
+						content: newMessage,
+						timestamp: getCurrentDateTime(),
+					};
+
+					setMessages([...messages, newMessageObject]);
+					setNewMessage('');
+				} else {
+					console.log('Error:', response.status);
+				}
+			} catch (error) {
+				console.log('An error occurred:', error);
 			}
-		} catch (error) {
-			console.log(`Error sending message:`, error);
+		}
+	};
+
+	const handleNewChatClick = () => {
+		setShowNewChatModal(true);
+	};
+
+	const handleNewChatNameChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNewChatName(event.target.value);
+	};
+
+	const handleSearchQueryChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setSearchQuery(event.target.value);
+	};
+
+	const handleSearchUsers = async () => {
+		if (searchQuery.trim() !== '') {
+			try {
+				const response = await fetch(
+					`http://localhost:5169/searchfriend?term=${searchQuery}`,
+					{
+						method: 'GET',
+						headers: {
+							Authorization: `Bearer ${sessionStorage.getItem('jwtToken')}`,
+							'Access-Control-Allow-Origin': '*',
+						},
+					}
+				);
+
+				if (response.ok) {
+					const data: any = await response.json();
+					console.log('Get successful:', data);
+					const parsedResults: UserInfo[] = data.map((item: string) => {
+						const [userid, username, phoneNumber, name, email] =
+							item.split(',');
+						return {
+							userid: Number(userid),
+							username,
+							phoneNumber,
+							name,
+							email,
+						};
+					});
+					console.log(parsedResults);
+					const filteredUsers = parsedResults.filter(
+						(user) =>
+							!selectedUsers.some(
+								(selectedUser) => selectedUser.userid === user.userid
+							)
+					);
+					setSearchedUsers(filteredUsers);
+				} else {
+					console.log('Error:', response.status);
+				}
+			} catch (error) {
+				console.log('An error occurred:', error);
+			}
+		}
+	};
+
+	const handleUserClick = (user: UserInfo) => {
+		setSelectedUsers((prevSelectedUsers) => [...prevSelectedUsers, user]);
+		setSearchedUsers([]);
+		setSearchQuery('');
+	};
+
+	const handleRemoveUser = (user: UserInfo) => {
+		setSelectedUsers((prevSelectedUsers) =>
+			prevSelectedUsers.filter((u) => u.userid !== user.userid)
+		);
+	};
+
+	const handleCreateChat = async () => {
+		if (newChatName.trim() !== '' && selectedUsers.length > 0) {
+			try {
+				const response = await fetch(
+					`http://localhost:5169/createconv?convname=${newChatName}&participantList=${selectedUsers
+						.map((user) => user.userid)
+						.toString()}`,
+					{
+						method: 'POST',
+						headers: {
+							Authorization: `Bearer ${sessionStorage.getItem('jwtToken')}`,
+						},
+					}
+				);
+
+				if (response.status === 200) {
+					const data: any = await response.json();
+					const newConversation: Conversation = {
+						id: data.convid,
+						name: newChatName,
+						lastMessage: '',
+						groupImage: '',
+					};
+
+					setConversations([...conversations, newConversation]);
+					setShowNewChatModal(false);
+					setNewChatName('');
+					setSelectedUsers([]);
+				} else {
+					console.log('Error:', response.status);
+				}
+			} catch (error) {
+				console.log('An error occurred:', error);
+			}
 		}
 	};
 
 	return (
-		<div
-			className='message-chat-container'
-			style={{
-				display: 'flex',
-				flexDirection: 'row',
-			}}
-		>
-			<div className='conversations'>
-				{conversations.map((conversation) => (
-					<div
-						key={conversation.id}
-						className={`conversation ${
-							selectedConversation === conversation ? 'active' : ''
-						}`}
-						onClick={() => handleConversationClick(conversation)}
-						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center',
-						}}
-					>
-						<div className='group-image'>
-							<img src='chat.jpg' height='30px' alt='Group Image' />
-						</div>
-						<div className='conversation-details'>
-							<div className='conversation-name'>
-								<b>{conversation.name}</b>
-							</div>
-							<div className='last-message'>Contributor1: Last Message</div>
-						</div>
-					</div>
-				))}
-			</div>
+		<div>
 			<div
-				className='messages'
-				style={{
-					marginLeft: '20px',
-					maxWidth: '100%',
-					flex: 1,
-					overflowY: 'auto',
-					maxHeight: 'calc(100vh - 20px)',
-				}}
-				ref={messagesContainerRef}
+				className='message-chat-container'
+				style={{ display: 'flex', flexDirection: 'row' }}
 			>
-				{selectedConversation ? (
-					<div className='conversation-messages'>
-						{messages.map((message) => (
-							<div
-								key={message.id}
-								className='message'
-								style={{
-									backgroundColor: '#ECE5DD',
-									marginBottom: '10px',
-									padding: '10px',
-									borderRadius: '5px',
-								}}
-							>
-								<div className='message-sender' style={{ fontWeight: 'bold' }}>
-									{message.sender}
-								</div>
-								<div className='message-content'>{message.content}</div>
-								<div
-									className='message-timestamp'
-									style={{ fontSize: '0.8rem', color: 'gray' }}
-								>
-									{message.timestamp}
-								</div>
-							</div>
-						))}
+				<div className='conversations'>
+					{conversations.map((conversation) => (
 						<div
-							className='message-input-container'
+							key={conversation.id}
+							className={`conversation ${
+								selectedConversation === conversation ? 'active' : ''
+							}`}
+							onClick={() => handleConversationClick(conversation)}
 							style={{
-								marginTop: '20px',
-								position: 'fixed',
-								// left: 0,
-								bottom: 0,
-								width: '100%',
-								backgroundColor: '#ffffff',
-								padding: '10px',
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
 							}}
 						>
+							<div className='group-image'>
+								<img
+									src={conversation.groupImage}
+									height='30px'
+									alt='Group Image'
+								/>
+							</div>
+							<div className='conversation-details'>
+								<div className='conversation-name'>
+									<b>{conversation.name}</b>
+								</div>
+								<div className='last-message'>{conversation.lastMessage}</div>
+							</div>
+						</div>
+					))}
+					<div
+						className='create-chat-button'
+						onClick={() => setShowNewChatModal(true)}
+					>
+						Create Chat
+					</div>
+				</div>
+				<div
+					className='messages'
+					style={{
+						marginLeft: '20px',
+						maxWidth: '100%',
+						flex: 1,
+						overflowY: 'auto',
+						maxHeight: 'calc(100vh - 20px)',
+					}}
+					ref={messagesContainerRef}
+				>
+					{selectedConversation ? (
+						<div className='conversation-messages'>
+							{messages.map((message) => (
+								<div
+									key={message.id}
+									className='message'
+									style={{
+										backgroundColor: '#ECE5DD',
+										marginBottom: '10px',
+										padding: '10px',
+										borderRadius: '5px',
+									}}
+								>
+									<div
+										className='message-sender'
+										style={{ fontWeight: 'bold' }}
+									>
+										{message.sender}
+									</div>
+									<div className='message-content'>{message.content}</div>
+									<div
+										className='message-timestamp'
+										style={{ fontSize: '0.8rem', color: 'gray' }}
+									>
+										{message.timestamp}
+									</div>
+								</div>
+							))}
+							<div
+								className='message-input-container'
+								style={{
+									marginTop: '20px',
+									position: 'fixed',
+									// left: 0,
+									bottom: 0,
+									width: '100%',
+									backgroundColor: '#ffffff',
+									padding: '10px',
+								}}
+							>
+								<input
+									type='text'
+									className='message-input'
+									placeholder='Type your message...'
+									value={newMessage}
+									onChange={(e) => setNewMessage(e.target.value)}
+								/>
+								<button onClick={handleSendMessage}>Send</button>
+							</div>
+						</div>
+					) : (
+						<h2>Select a Conversation</h2>
+					)}
+				</div>
+				{selectedConversation && (
+					<div className='chat-footer'>
+						<input
+							type='text'
+							placeholder='Type a message...'
+							value={newMessage}
+							onChange={handleNewMessageChange}
+						/>
+						<button onClick={handleSendMessage}>Send</button>
+					</div>
+				)}
+			</div>
+			<div>
+				{showNewChatModal && (
+					<div className='new-chat-modal'>
+						<div className='modal-content'>
+							<h2>Create New Chat</h2>
 							<input
 								type='text'
-								className='message-input'
-								placeholder='Type your message...'
-								value={newMessage}
-								onChange={(e) => setNewMessage(e.target.value)}
+								placeholder='Chat Name'
+								value={newChatName}
+								onChange={handleNewChatNameChange}
 							/>
-							<button className='send-button' onClick={handleSendMessage}>
-								<i className='fa fa-paper-plane'></i>
-							</button>
+							<div className='search-users'>
+								<input
+									type='text'
+									placeholder='Search Users'
+									value={searchQuery}
+									onChange={handleSearchQueryChange}
+								/>
+								<button onClick={handleSearchUsers}>Search</button>
+								{searchedUsers.length > 0 && (
+									<ul>
+										{searchedUsers.map((user) => (
+											<li
+												key={user.userid}
+												onClick={() => handleUserClick(user)}
+											>
+												{user.username}
+											</li>
+										))}
+									</ul>
+								)}
+							</div>
+							<div className='selected-users'>
+								<h3>Selected Users:</h3>
+								<ul>
+									{selectedUsers.map((user) => (
+										<li key={user.userid}>
+											{user.username}
+											<button onClick={() => handleRemoveUser(user)}>
+												Remove
+											</button>
+										</li>
+									))}
+								</ul>
+							</div>
+							<div className='create-chat-buttons'>
+								<button onClick={handleCreateChat}>Create</button>
+								<button onClick={() => setShowNewChatModal(false)}>
+									Cancel
+								</button>
+							</div>
 						</div>
-					</div>
-				) : (
-					<div className='no-conversation-selected'>
-						Select a conversation to view messages.
 					</div>
 				)}
 			</div>
